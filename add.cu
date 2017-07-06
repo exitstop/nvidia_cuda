96ba002f8d6f4b2b9615d9afe85cf8027b504058
exitstop <exitsto@list.ru>
2017-07-06 20:13:02 +0300

    first commit

---
+++ add.cu
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+__global__ void cube(int * d_out, int * d_in){
+    int idx = threadIdx.x;
+    for(int i = 0; i < 10000000; i++){
+        int f = d_in[idx];
+        d_out[idx] = f+1;
+    }
+}
+
+int main(int argc, char ** argv) {
+    const int ARRAY_SIZE = 328;
+    const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
+
+    // generate the input array on the host
+    int h_in[ARRAY_SIZE];
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        h_in[i] = i;
+    }   
+    int h_out[ARRAY_SIZE];
+
+    // declare GPU memory pointers
+    int * d_in;
+    int * d_out;
+
+    // allocate GPU memory
+    cudaMalloc((void**) &d_in, ARRAY_BYTES);
+    cudaMalloc((void**) &d_out, ARRAY_BYTES);
+
+    // transfer the array to the GPU 
+    cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+
+    // launch the kernel
+    cube<<<1, ARRAY_SIZE>>>(d_out, d_in);
+
+    // copy back the result array to the CPU 
+    cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+
+    // print out the resulting array
+    //for (int i =0; i < ARRAY_SIZE; i++) {
+    //    printf("%d", h_out[i]);
+    //    printf(((i % 4) != 3) ? "\t" : "\n");
+    //}   
+
+    cudaFree(d_in);
+    cudaFree(d_out);
+
+    return 0;
+}
